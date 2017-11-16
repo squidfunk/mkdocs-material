5c8d241d6af395a92b42ac0c75b74b6843885dfa
Max <m4ks@o2.pl>
2017-11-16 23:26:58 +0100

    Removed old comment

--- material/assets/javascripts/script.js
+++ material/assets/javascripts/script.js
@@ -492,7 +492,6 @@
     // it prevents focusing on other elements that are NOT in drawer
     if (document !== ev.target && sidebarPrimary !== ev.target && !sidebarPrimary.contains(ev.target)) {
       sidebarPrimary.querySelector('[tabindex="0"]').focus();
-      // when drawer is opened, you are in the middle of the page and you are tabbing through links in drawer - when sidebar primary gets focus it will jump to the top of the page so we can read top scroll position on each TAB key press and scroll to that position to prevent jumping
     }
   }
 
