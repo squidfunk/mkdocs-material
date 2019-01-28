2f31618d607516113553f714f69a8831d626d636
squidfunk <martin.donath@squidfunk.com>
2019-01-28 11:13:26 +0100

    Use asynchronous Google Analytics tracking

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -288,9 +288,7 @@ function initialize(config) { // eslint-disable-line func-style
     new Material.Event.Listener("[data-md-component=query]", [
       "focus", "keyup", "change"
     ], new Material.Search.Result("[data-md-component=result]", () => {
-      return fetch(`${config.url.base}/${
-        config.version < "0.17" ? "mkdocs" : "search"
-      }/search_index.json`, {
+      return fetch(`${config.url.base}/search/search_index.json`, {
         credentials: "same-origin"
       }).then(response => response.json())
         .then(data => {
