6f85ad5dd595d0f8bf741e02aa4fef36231b7ed9
squidfunk <martin.donath@squidfunk.com>
2020-04-26 13:55:33 +0200

    Removed css-mqpacker PostCSS plugin
    The plugin was deprecated some time ago and may yield unintended side-effects
    due to the re-ordering of media queries.

--- webpack.config.ts
+++ webpack.config.ts
@@ -126,8 +126,7 @@ function config(args: Configuration): Configuration {
               options: {
                 ident: "postcss",
                 plugins: () => [
-                  require("autoprefixer")(),
-                  require("css-mqpacker")
+                  require("autoprefixer")()
                 ],
                 sourceMap: true
               }
