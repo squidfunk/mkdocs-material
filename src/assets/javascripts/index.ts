f32778cda2ecdf7f3036272c088aeb8a5e1959dc
squidfunk <martin.donath@squidfunk.com>
2020-06-20 12:21:34 +0200

    Fixed duplication of search worker when subscribing to observable

--- src/assets/javascripts/index.ts
+++ src/assets/javascripts/index.ts
@@ -292,14 +292,14 @@ export function initialize(config: unknown) {
         return useComponent("search")
           .pipe(
             mountSearch(worker, { query$, reset$, result$ }),
-            shareReplay(1)
           )
       }),
       catchError(() => {
         useComponent("search")
           .subscribe(el => el.hidden = true) // TODO: Hack
         return NEVER
-      })
+      }),
+      shareReplay(1)
     )
 
   /* ----------------------------------------------------------------------- */
