89a60c39e73c1484584fa5c1b0f75f07018bc3ae
squidfunk <martin.donath@squidfunk.com>
2020-08-30 12:23:34 +0200

    Moved clipboard icon to CSS to reduce size of large projects

--- src/assets/javascripts/templates/clipboard/index.tsx
+++ src/assets/javascripts/templates/clipboard/index.tsx
@@ -33,15 +33,6 @@ const css = {
   container: "md-clipboard md-icon"
 }
 
-/* ------------------------------------------------------------------------- */
-
-/**
- * Path of `file-search-outline` icon
- */
-const path =
-  "M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 " +
-  "21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"
-
 /* ----------------------------------------------------------------------------
  * Functions
  * ------------------------------------------------------------------------- */
@@ -61,10 +52,6 @@ export function renderClipboardButton(
       class={css.container}
       title={translate("clipboard.copy")}
       data-clipboard-target={`#${id} > code`}
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d={path}></path>
-      </svg>
-    </button>
+    ></button>
   )
 }
