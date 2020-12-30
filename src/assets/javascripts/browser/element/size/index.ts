b825d222a4db8c63d6822377eafbe347611b3405
squidfunk <martin.donath@squidfunk.com>
2020-12-30 18:40:34 +0100

    Refactored resize observer

--- src/assets/javascripts/browser/element/size/index.ts
+++ src/assets/javascripts/browser/element/size/index.ts
@@ -26,7 +26,6 @@ import {
   Observable,
   Subject,
   defer,
-  merge,
   of
 } from "rxjs"
 import {
@@ -77,7 +76,7 @@ const observer$ = defer(() => of(
   })
 ))
   .pipe(
-    switchMap(resize => merge(of(resize), NEVER)
+    switchMap(resize => NEVER.pipe(startWith(resize))
       .pipe(
         finalize(() => resize.disconnect())
       )
