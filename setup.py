16bb4379d1868ef1ae8ac3f8ee0b796657e56422
Ofek Lev <ofekmeister@gmail.com>
2022-09-18 11:44:18 -0400

    Modernized metadata (#4341)
    * Revert "Revert "Switched to new build and publish workflow (#4337)" (#4340)"
    
    This reverts commit 1a9bc1dd6aa5c4788d24745684ed0324133091d2.
    
    * Create setup.py
    
    * update

--- setup.py
+++ setup.py
@@ -1,79 +1,32 @@
-# Copyright (c) 2016-2022 Martin Donath <martin.donath@squidfunk.com>
+import sys
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+from setuptools import setup
 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+sys.stderr.write(
+    """
+===============================
+Unsupported installation method
+===============================
+This version of mkdocs-material no longer supports
+installation with `python setup.py install`.
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
 
-import json
-from setuptools import setup, find_packages
 
-# Load package.json contents
-with open("package.json") as f:
-    package = json.load(f)
+# The below code will never execute, however GitHub is particularly
+# picky about where it finds Python packaging metadata.
+# See: https://github.com/github/feedback/discussions/6456
 
-# Load list of dependencies
 with open("requirements.txt") as f:
     install_requires = [
         line for line in f.read().split("\n")
         if line and not line.startswith("#")
     ]
 
-# Load README contents
-with open("README.md", encoding = "utf-8") as f:
-    long_description = f.read()
-
-# Package description
 setup(
-    name = "mkdocs-material",
-    version = package["version"],
-    url = package["homepage"],
-    project_urls = {
-        "Source": "https://github.com/squidfunk/mkdocs-material",
-    },
-    license = package["license"],
-    description = package["description"],
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    author = package["author"]["name"],
-    author_email = package["author"]["email"],
-    keywords = package["keywords"],
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: JavaScript",
-        "Programming Language :: Python",
-        "Topic :: Documentation",
-        "Topic :: Software Development :: Documentation",
-        "Topic :: Text Processing :: Markup :: HTML"
-    ],
-    packages = find_packages(exclude = ["src", "src.*"]),
-    include_package_data = True,
-    install_requires = install_requires,
-    python_requires='>=3.7',
-    entry_points = {
-        "mkdocs.themes": [
-            "material = material"
-        ],
-        "mkdocs.plugins": [
-            "search = material.plugins.search.plugin:SearchPlugin",
-            "social = material.plugins.social.plugin:SocialPlugin",
-            "tags = material.plugins.tags.plugin:TagsPlugin"
-        ]
-    },
-    zip_safe = False
+    name="mkdocs-material",
+    install_requires=install_requires,
 )
