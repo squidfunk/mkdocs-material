cafd67b7385077233ab2b9f7b245080c96a986d2
squidfunk <martin.donath@squidfunk.com>
2024-08-23 12:30:51 +0200

    Fixed error in build script

--- tools/build/index.ts
+++ tools/build/index.ts
@@ -92,9 +92,17 @@ function minsvg(data: string): string {
   /* Optimize SVG */
   const result = optimize(data, {
     plugins: [
-      "preset-default",
-      { name: "removeDimensions", active: true },
-      { name: "removeViewBox", active: false }
+      {
+        name: "preset-default",
+        params: {
+          overrides: {
+            removeViewBox: false
+          }
+        }
+      },
+      {
+        name: "removeDimensions"
+      }
     ]
   })
 
