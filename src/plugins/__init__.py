70dee38366615652d7198ae85ff208a4b4c0e80d
squidfunk <martin.donath@squidfunk.com>
2026-02-20 12:03:41 +0100

    Switched warning message to print to stderr

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -19,16 +19,20 @@
 # IN THE SOFTWARE.
 
 from os import getenv
+from sys import stderr
 from colorama import Fore, Style
 
 # Silence this warning with NO_MKDOCS_2_WARNING=1
 if not getenv("NO_MKDOCS_2_WARNING"):
-    print("")
-    print(f"{Fore.RED} │ ⚠ WARNING – MkDocs 2.0 is incompatible with Material for MkDocs{Style.RESET_ALL}")
-    print(f"{Fore.RED} │ {Style.RESET_ALL}")
-    print(f"{Fore.RED} │ {Style.RESET_ALL}  MkDocs 1.x is unmaintained. We recommend switching to Zensical, our")
-    print(f"{Fore.RED} │ {Style.RESET_ALL}  new static site generator, as soon as possible. We're providing an")
-    print(f"{Fore.RED} │ {Style.RESET_ALL}  analysis of the situation in this article:")
-    print(f"{Fore.RED} │ {Style.RESET_ALL}  ")
-    print(f"{Fore.RED} │ {Style.RESET_ALL}  \033[4mhttps://squidfunk.github.io/mkdocs-material/blog/2026/02/18/mkdocs-2.0/{Style.RESET_ALL}")
-    print(f"{Style.RESET_ALL}")
+    print(
+        "\n"
+        f"{Fore.RED} │ ⚠ WARNING – MkDocs 2.0 is incompatible with Material for MkDocs{Style.RESET_ALL}\n"
+        f"{Fore.RED} │ {Style.RESET_ALL}\n"
+        f"{Fore.RED} │ {Style.RESET_ALL}  MkDocs 1.x is unmaintained. We recommend switching to Zensical, our\n"
+        f"{Fore.RED} │ {Style.RESET_ALL}  new static site generator, as soon as possible. We're providing an\n"
+        f"{Fore.RED} │ {Style.RESET_ALL}  analysis of the situation in this article:\n"
+        f"{Fore.RED} │ {Style.RESET_ALL}  \n"
+        f"{Fore.RED} │ {Style.RESET_ALL}  \033[4mhttps://squidfunk.github.io/mkdocs-material/blog/2026/02/18/mkdocs-2.0/{Style.RESET_ALL}\n"
+        f"{Style.RESET_ALL}",
+        file=stderr
+    )
